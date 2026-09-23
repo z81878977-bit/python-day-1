03fc263f04e67362fc732fe9bf463f5432dcd078
Xinyu Zhang <z81878977@gmail.com>
2026-09-23 18:44:23 -0400

    complete pyyhon-day-1

---
+++ logic.py
@@ -0,0 +1,11 @@
+def celsius_to_fahrenheit(celsius):
+    #write later
+    return fahrenheit
+
+def calculate_sales_tax(price):
+    #write later
+    return sales_tax
+
+def pythagorean(a,b):
+    #write later
+    return hypotenuse
---
+++ shelfer.py
@@ -0,0 +1,13 @@
+def adopt_cats(cat):
+    #I'll write this later
+    print("Enjoy your cat!")
+    return None 
+
+def feed_cats(food):
+    #I'll write this later
+    return None
+
+def scoop_litter():
+    #I'll write this later
+    return cat_poop
+
